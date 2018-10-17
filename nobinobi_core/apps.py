cfaf1820b3b24dade7ab17a00d2855be100ca483
Florian Alu <webmaster@prolibre.com>
2018-10-17 19:48:13 +0200

    Add verbose name on apps

--- nobinobi_core/apps.py
+++ nobinobi_core/apps.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class NobinobiCoreConfig(AppConfig):
     name = 'nobinobi_core'
+    verbose_name = _("Core")
+    verbose_name_plural = _("Core")
